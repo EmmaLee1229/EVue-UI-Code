9745a994a9feb957bc52f008398513fd119c59b0
EmmaLee <15830586201@163.com>
2021-02-22 17:48:21 +0800

    可以实现switch切换

---
+++ src/components/SwitchDemo.vue
@@ -0,0 +1,16 @@
+<template>
+    <Switch/>
+</template>
+
+<script lang="ts">
+    import Switch from '../lib/Switch.vue'
+    export default {
+        components:{
+            Switch
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
