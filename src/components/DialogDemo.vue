e1ae8b3a1c351c2a12ec88c72812f66434b4d233
EmmaLee <15830586201@163.com>
2021-02-23 18:05:55 +0800

    实现dialog的点击事件功能

---
+++ src/components/DialogDemo.vue
@@ -0,0 +1,38 @@
+<template>
+    <h1>对话框示例</h1>
+    <Button @click="toggle">点击打开对话框</Button>
+    <Dialog :isvisible="visible" @update:isvisible="visible=$event" :closeOnclickOverlay="false" :yes="yes" :no="no"></Dialog>
+</template>
+
+<script lang="ts">
+    import Dialog from "../lib/Dialog.vue";
+    import Button from "../lib/Button.vue";
+    import { ref } from "vue";
+
+    export default {
+        components:{
+            Dialog:Dialog,
+            Button:Button
+        },
+        setup(){
+            const visible=ref(false)
+            const toggle =()=>{
+                visible.value=!visible.value
+            };
+            const yes=()=>{
+                return false
+            }
+            const no =()=>{
+                return true
+            }
+            return {
+                visible,toggle,yes,no
+            }
+        }
+    }
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
